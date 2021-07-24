13ca8373cca949800357d8566d77501913c430b2
gsniper777 <gsniper777@gmail.com>
2021-07-24 15:16:23 +0900

    0724

--- mysite/pytest/views.py
+++ mysite/pytest/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 #from django.http import HttpResponse
+from django.utils import timezone
 from .models import Question
+from .forms import QuestionForm
+
 
 # Create your views here.
 
@@ -15,3 +18,22 @@ def detail(request,question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pytest/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    #answer = Answer(question=question, content=request.POST.get('content'),create_date=timezone.now())
+    question.answer_set.create(content = request.POST.get('content'),create_date=timezone.now())
+    return redirect('pytest:detail',question_id=question.id)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question=form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pytest:index')
+    else:
+        form = QuestionForm()
+    context = {'form':form}
+    return render(request, 'pytest/question_form.html', context)
